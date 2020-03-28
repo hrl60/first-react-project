a003b7cbae932d068814c8908d215a371f1e3fe5
hrl60 <hrl60@yahoo.com>
2020-03-28 05:54:56 +0430

    Updates

---
+++ precache-manifest.bc381d09f9bb41433a7b482dc56a2267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331a314cf4bf9d61c069009bcf2da91c",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "d5402c7522c07a345d71",
+    "url": "/first-react-project/static/css/main.9177b30a.chunk.css"
+  },
+  {
+    "revision": "527adc84708e36710de8",
+    "url": "/first-react-project/static/js/2.a5d491aa.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/first-react-project/static/js/2.a5d491aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5402c7522c07a345d71",
+    "url": "/first-react-project/static/js/main.6967ef72.chunk.js"
+  },
+  {
+    "revision": "979e5bc7c2f05ba2b00d",
+    "url": "/first-react-project/static/js/runtime-main.c8fb5280.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
